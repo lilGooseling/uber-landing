9f52656413208f74aa84a7fb624c0b3dfdbdd8db
Наталья Оскарова <gooseberry.bay@gmail.com>
2022-03-12 22:16:50 +0300

    Работа с Gulp, часть 3

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync');
 const sass        = require('gulp-sass')(require('sass'));
+const rename = require("gulp-rename");
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 
 // Static server
 gulp.task('server', function() {
@@ -14,6 +17,14 @@ gulp.task('server', function() {
 gulp.task('styles', function () {
     return gulp.src("src/sass/*.+(scss|sass)")
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({
+            prefix: "",
+            suffix: ".min",
+        }))
+        .pipe(autoprefixer({
+            cascade: false
+        }))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest("src/sass"))
         .pipe(browserSync.stream());
 });
